1d647a5652a34b9663ab9c67f532dc47ea9119d2
NSUWAL123 <suwalnishit@gmail.com>
2022-08-23 09:50:53 +0545

    Updated UI

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link, Outlet} from 'react-router-dom'
+import logo from "../pictures/logo.jpg"
+import { Icon } from '@iconify/react';
+import '../styles/BaseLayout.css'
 
 function BaseLayout(){
   return (
-    <div>
-        <Link to="/">
+    <div className='main-container-bl'>
+        <Link to="/" className='container-bl'>
+          <Icon icon="healthicons:syringe-vaccine" color="#ff002f" className='icon' width="80px"/>
           <h2>VAS</h2>
         </Link>
+        <hr className='hr-bl'/>
         <Outlet />
     </div>
   )
